dc08a81862521ddbd55b21f814a90ec9c34f90b3
sewwwandifernando <sewwandinirushika7@gmail.com>
2023-09-10 16:32:15 +0530

    Edited patient.js file.

--- server/models/Patients.js
+++ server/models/Patients.js
@@ -1,5 +1,3 @@
-
-
 module.exports = (sequelize, DataTypes) => {
     const Patients = sequelize.define("Patients", {
       hospitalId: {
@@ -14,6 +12,51 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      date_Of_Birth: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      contactNo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      address: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      nic: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      bloodGroup: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      guardianName: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      guardian_contactNo: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      guardianAddress: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      statusId: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+      }
+      
+
+      
+
+
     });
     return Patients;
 
--- server/service/patient.service.js
+++ server/service/patient.service.js
@@ -10,7 +10,7 @@ async function registerPatient(patient) {
         throw error;
     }
     
-}
+}   
 
 async function getAllPatients(){
     try {
